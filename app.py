53f2bba85fdb54fc35827d6b74b9b13b9e6baea9
Jeff Chen <jeffc@Soma-3.local>
2023-11-03 20:01:00 -0500

    added some model and views testing

--- app.py
+++ app.py
@@ -319,6 +319,10 @@ def messages_show(message_id):
     """Show a message."""
 
     msg = Message.query.get(message_id)
+
+    if msg is None:
+        return abort(404)
+
     return render_template('messages/show.html', message=msg)
 
 
@@ -330,7 +334,11 @@ def messages_destroy(message_id):
         flash("Access unauthorized.", "danger")
         return redirect("/")
 
-    msg = Message.query.get(message_id)
+    msg = Message.query.get_or_404(message_id)
+    if msg.user_id != g.user.id:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+
     db.session.delete(msg)
     db.session.commit()
 
